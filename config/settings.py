7293df6e8cac9aa14991960d527d238b4cf5bee2
Enes Aksoy <must.enesaks@gmail.com>
2026-06-04 14:23:22 +0300

    feat: admin custom css via static file

--- config/settings.py
+++ config/settings.py
@@ -218,7 +218,7 @@ JAZZMIN_SETTINGS = {
     "default_icon_parents": "fas fa-folder",
     "default_icon_children": "fas fa-dot-circle",
     "related_modal_active": True,
-    "custom_css": None,
+    "custom_css": "css/admin_custom.css",
     "custom_js": None,
     "use_google_fonts_cdn": False,
     "show_ui_builder": False,
